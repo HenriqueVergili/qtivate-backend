6f72294f4bd9d08356bc22f8ea6f1fb864ea66fb
charles <arianepaulabarros@gmail.com>
2022-12-01 09:27:18 -0300

    add cors config class

---
+++ src/main/java/com/qtivate/server/CorsConfiguration.java
@@ -0,0 +1,32 @@
+package com.qtivate.server;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration
+{
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000","https://localhost:3000");
+            }
+        };
+    }
+}
+/*@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer
+{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000","https://localhost:3000")
+                .allowedMethods("GET", "POST");
+    }
+}*/
